6af63c01da757d171028e98431ee165373e34118
henry <henry@DESKTOP-SGQGF39>
2021-09-27 18:43:34 -0500

    Avance sesion 2

--- src/main/java/com/empresa/controller/AlumnoController.java
+++ src/main/java/com/empresa/controller/AlumnoController.java
@@ -1,11 +1,16 @@
 package com.empresa.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,9 +45,60 @@ public class AlumnoController {
 		}
 	}
 	
+	@PutMapping
+	@ResponseBody
+	public ResponseEntity<Alumno> actualizaAlumno (@RequestBody Alumno obj){
+		if(obj == null) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			Optional<Alumno> optAlumno = service.buscaPorId(obj.getIdAlumno());
+			if(optAlumno.isPresent()) {
+				Alumno objActualizado = service.insertaActualizaAlumno(obj);
+				return ResponseEntity.ok(objActualizado);
+			}else {
+				return ResponseEntity.badRequest().build();
+			}
+		}
+	}
 	
+	@DeleteMapping("/{paramId}")
+	@ResponseBody
+	public ResponseEntity<Alumno> eliminaAlumno(@PathVariable("paramId") int idAlumno){
+		Optional<Alumno> optAlumno = service.buscaPorId(idAlumno);
+		if (optAlumno.isPresent()) {
+			service.eliminaPorId(idAlumno);
+			Optional<Alumno> optEliminado = service.buscaPorId(idAlumno);
+			if (optEliminado.isPresent()) {
+				return ResponseEntity.badRequest().build();
+			}else {
+				return ResponseEntity.ok(optAlumno.get());
+			}
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
+		
+	@GetMapping("/id/{paramId}")
+	@ResponseBody
+	public ResponseEntity<Alumno> listaAlumnoPorId(@PathVariable("paramId")int idAlumno){
+		Optional<Alumno> optAlumno = service.buscaPorId(idAlumno);
+		if(optAlumno.isPresent()) {
+			return ResponseEntity.ok(optAlumno.get());
+		}else {
+			return ResponseEntity.badRequest().build();
+		}
+	}
 	
+	@GetMapping("/dni/{paramDni}")
+	@ResponseBody
+	public ResponseEntity<List<Alumno>> listaAlumnoPorDni(@PathVariable("paramDni")String dni){
+		List<Alumno> lista = service.listaAlumnoPorDni(dni);
+		if(CollectionUtils.isEmpty(lista)) {
+			return ResponseEntity.badRequest().build();
+		}else {
+			return ResponseEntity.ok(lista);
+		}
+	}
 	
-	
-	
+
 }
--- src/main/java/com/empresa/entity/Alumno.java
+++ src/main/java/com/empresa/entity/Alumno.java
@@ -12,13 +12,10 @@ import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Table(name = "alumno")
-@Getter
-@Setter
+
 public class Alumno {
 
 	@Id
@@ -30,6 +27,38 @@ public class Alumno {
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	@Temporal(TemporalType.DATE)
 	private Date fechaNacimiento;
+	
+	public int getIdAlumno() {
+		return idAlumno;
+	}
+	public void setIdAlumno(int idAlumno) {
+		this.idAlumno = idAlumno;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	
 	
 	
 }
